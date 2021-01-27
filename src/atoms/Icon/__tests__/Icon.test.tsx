68f7887ccee85c7308dff67bb375bf557e0af7e8
duytq <duybkit13@gmail.com>
2021-01-27 17:04:08 +0700

    Feat: Fix test Icon

--- src/atoms/Icon/__tests__/Icon.test.tsx
+++ src/atoms/Icon/__tests__/Icon.test.tsx
@@ -7,18 +7,8 @@ import '@testing-library/jest-dom/extend-expect'
 test('loads and displays greeting', async () => {
   render(<Icon />)
 
-  fireEvent.click(screen.getByText('Load Greeting'))
+  fireEvent.click(screen.getByText('haha'))
 
   await waitFor(() => screen.getByRole('heading'))
   
-})
-
-test('handles server error', async () => {
- 
-  render(<Icon />)
-
-  fireEvent.click(screen.getByText('Load Greeting'))
-
-  await waitFor(() => screen.getByRole('alert'))
-
 })
