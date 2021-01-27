71bde059683aaea9d299cc37d9385a4fdfca82f5
duytq <duybkit13@gmail.com>
2021-01-27 08:57:29 +0700

    feat: reconfig storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,10 @@
-const path = require("path");
-
-// Export a function. Accept the base config as the only param.
 module.exports = {
-  typescript: {
-    check: false,
-    checkOptions: {},
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
-    },
-  },
-};
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
---
+++ src/atom/Icon/Icon.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+const Icon: Story<any> = () => (<h1>jaajak</h1>)
+
+export default {
+    title: '123/Header',
+    component: Icon,
+  } as Meta;
+  
