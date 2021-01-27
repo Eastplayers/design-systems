da5f1531a0b69574f7167475054b31be8edc24e4
duytq <duybkit13@gmail.com>
2021-01-27 17:33:28 +0700

    Feat: Remove unused code

--- src/atoms/Icon/__tests__/Icon.test.tsx
+++ src/atoms/Icon/__tests__/Icon.test.tsx
@@ -1,7 +1,7 @@
 // __tests__/Icon.test.js
 import React from 'react'
 import Icon from '../Icon'
-import { render, fireEvent, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 test('<Icon /> expect return haha', async () => {
