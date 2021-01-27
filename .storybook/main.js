029caba7a62f1c8dc8a8e3063865306cd339a9cd
duytq <duybkit13@gmail.com>
2021-01-27 07:21:32 +0700

    feat: integrate storybook

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+// Export a function. Accept the base config as the only param.
+module.exports = {
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+};
