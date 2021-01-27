c49cf1f101398fdcb8bfa873fc7fae6f791487e9
duytq <duybkit13@gmail.com>
2021-01-27 17:14:32 +0700

    Feat: Update test file

--- src/atoms/Icon/__tests__/Icon.test.tsx
+++ src/atoms/Icon/__tests__/Icon.test.tsx
@@ -4,11 +4,8 @@ import Icon from '../Icon'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
-test('loads and displays greeting', async () => {
+test('<Icon /> expect return haha', async () => {
   render(<Icon />)
 
   fireEvent.click(screen.getByText('haha'))
-
-  await waitFor(() => screen.getByRole('heading'))
-  
 })
