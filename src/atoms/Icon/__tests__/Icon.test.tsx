68a60479dd8867c154899845e29667369685b4ad
duytq <duybkit13@gmail.com>
2021-01-27 17:15:31 +0700

    Feat: Update test Icon component

--- src/atoms/Icon/__tests__/Icon.test.tsx
+++ src/atoms/Icon/__tests__/Icon.test.tsx
@@ -1,11 +1,11 @@
 // __tests__/Icon.test.js
 import React from 'react'
 import Icon from '../Icon'
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 test('<Icon /> expect return haha', async () => {
   render(<Icon />)
 
-  fireEvent.click(screen.getByText('haha'))
+  screen.getByText('haha');
 })
