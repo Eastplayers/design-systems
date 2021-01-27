908fb6abae53a93034686804155927254290ba84
Le Tu Quyen <quyen.le@eastplayers.io>
2021-01-27 11:37:11 +0700

    Add template in story file

--- src/atom/Icon/Icon.stories.tsx
+++ src/atom/Icon/Icon.stories.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
 
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 
-const Icon: Story<any> = () => (<h1>jaajak</h1>)
+const Icon: Story<React.Component> = () => <h1>jaajak</h1>;
 
 export default {
-    title: '123/Header',
-    component: Icon,
-  } as Meta;
-  
+  title: "123/Header",
+  component: Icon,
+} as Meta;
+
+const Template: Story<any> = (args) => <Icon {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
