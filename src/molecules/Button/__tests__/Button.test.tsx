cb0af12a39a174704df46f19b45ac5a212eb0c6d
duytq <duybkit13@gmail.com>
2021-01-31 00:39:29 +0700

    update test of primary button

--- src/molecules/Button/__tests__/Button.test.tsx
+++ src/molecules/Button/__tests__/Button.test.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import Button from "../Button";
+import { ButtonVariants, ButtonSizes } from "../types";
 import { render } from "@testing-library/react";
 
 import "@testing-library/jest-dom/extend-expect";
 
-describe("Button unit test", () => {
-  test("<Button /> expect has Submit text", async () => {
-    const { getByText } = render(<Button label={"Submit"} />);
+test("Button expect has Submit text", async () => {
+  const { getByText } = render(<Button label={"Submit"} />);
 
-    expect(getByText("Submit")).toBeInTheDocument();
-    expect(getByText("Submit")).toHaveTextContent("Submit");
-  });
+  expect(getByText("Submit")).toBeInTheDocument();
+  expect(getByText("Submit")).toHaveTextContent("Submit");
+});
+
+test("Primary Button expect has btn--primary class", async () => {
+  const { getByRole } = render(
+    <Button label={"Submit"} variant={ButtonVariants.Primary} />
+  );
+
+  expect(getByRole("button")).toHaveClass("btn--primary");
+});
+
+test("Secondary Button size L expect has Heading 5", async () => {
+  const { getByText } = render(
+    <Button
+      label={"Submit"}
+      variant={ButtonVariants.Secondary}
+      size={ButtonSizes.L}
+    />
+  );
+
+  expect(getByText("Submit")).toHaveClass("heading-4");
 });
