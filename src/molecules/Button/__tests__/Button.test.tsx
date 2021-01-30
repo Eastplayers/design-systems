dfc23a6b723a305ca70d74ede9f4ce0403b11a20
duytq <duybkit13@gmail.com>
2021-01-30 18:47:25 +0700

    Fix test fail

--- src/molecules/Button/__tests__/Button.test.tsx
+++ src/molecules/Button/__tests__/Button.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Button from "../Button";
+import { ButtonSizes, ButtonVariants } from "../types";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 test("<Icon /> expect return haha", async () => {
-  render(<Button />);
+  render(<Button label={"haha"} />);
 
   screen.getByText("haha");
 });
