ab42c4804ca4bfebd6066dde6f752485ee1d0e9c
duytq <duybkit13@gmail.com>
2021-01-31 00:20:55 +0700

    Fix test button

--- src/molecules/Button/__tests__/Button.test.tsx
+++ src/molecules/Button/__tests__/Button.test.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Button from "../Button";
 import { render } from "@testing-library/react";
 
+import "@testing-library/jest-dom/extend-expect";
+
 describe("Button unit test", () => {
   test("<Button /> expect has label text", async () => {
     const { getByText } = render(<Button label={"label"} />);
 
-    expect(getByText("label"));
+    expect(getByText("label")).toBeInTheDocument();
   });
 });
