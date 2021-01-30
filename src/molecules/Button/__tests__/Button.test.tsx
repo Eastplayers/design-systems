e86be405adc184b4088353c5f066b326cf21107d
duytq <duybkit13@gmail.com>
2021-01-30 18:57:47 +0700

    update PR

--- src/molecules/Button/__tests__/Button.test.tsx
+++ src/molecules/Button/__tests__/Button.test.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Button from "../Button";
-import { ButtonSizes, ButtonVariants } from "../types";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-test("<Icon /> expect return haha", async () => {
+test("<Button /> expect has haha text", async () => {
   render(<Button label={"haha"} />);
 
   screen.getByText("haha");
