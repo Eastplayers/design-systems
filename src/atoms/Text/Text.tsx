3b1df5f78b67f61f08d6dd27a724552b634b9894
duytq <duybkit13@gmail.com>
2021-01-30 17:03:04 +0700

    expose types for Text component

--- src/atoms/Text/Text.tsx
+++ src/atoms/Text/Text.tsx
@@ -1,34 +1,8 @@
 import React from "react";
 import ClassNames from "classnames";
+
+import { FontTypes, FontWeights, TextDecorations } from "./types";
 import styles from "./Text.scss";
-
-export enum FontTypes {
-  BODY_1 = "body-1",
-  BODY_2 = "body-2",
-  BODY_3 = "body-3",
-  BODY_4 = "body-4",
-  HEADING_1 = "heading-1",
-  HEADING_2 = "heading-2",
-  HEADING_3 = "heading-3",
-  HEADING_4 = "heading-4",
-  HEADING_5 = "heading-5",
-  HEADING_6 = "heading-6",
-  DISPLAY_1 = "display-1",
-  DISPLAY_2 = "display-2",
-  DISPLAY_3 = "display-3"
-}
-
-export enum FontWeights {
-  NORMAL = "400",
-  BOLD = "700",
-  EXTRA_BOLD = "800"
-}
-
-export enum TextDecorations {
-  NONE = "none",
-  UNDERLINE = "underline",
-  LINE_THROUGH = "line-through"
-}
 
 export interface TextProps {
   children: React.ReactNode | string;
