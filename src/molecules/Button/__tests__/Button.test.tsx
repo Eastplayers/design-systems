6ee6717aace7a1f36cac2bfe1c0e9ad3447c6809
duytq <duybkit13@gmail.com>
2021-01-31 00:23:30 +0700

    update test button

--- src/molecules/Button/__tests__/Button.test.tsx
+++ src/molecules/Button/__tests__/Button.test.tsx
@@ -5,9 +5,10 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
 describe("Button unit test", () => {
-  test("<Button /> expect has label text", async () => {
-    const { getByText } = render(<Button label={"label"} />);
+  test("<Button /> expect has Submit text", async () => {
+    const { getByText } = render(<Button label={"Submit"} />);
 
-    expect(getByText("label")).toBeInTheDocument();
+    expect(getByText("Submit")).toBeInTheDocument();
+    expect(getByText("Submit")).toHaveTextContent("Submit");
   });
 });
