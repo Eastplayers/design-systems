9ee901bdb961b09bdf27b994dec63ced320ac2f9
esp-phudang <phu.dang@eastplayers.io>
2021-02-02 12:47:43 +0700

    layout component

--- src/atoms/Icon/Icon.stories.tsx
+++ src/atoms/Icon/Icon.stories.tsx
@@ -6,6 +6,7 @@ import "@storybook/addon-knobs/register";
 import { Icon } from ".";
 import { IconProps } from "./Icon";
 import iconSet from "./selection.json";
+import "./Icon.scss";
 
 export const Icons = (): React.ReactElement<IconProps> => {
   // const iconList = select("Icon", IconList, IconList.LIKE);
@@ -21,20 +22,46 @@ export const Icons = (): React.ReactElement<IconProps> => {
     <div style={{ display: "flex", flexWrap: "wrap" }}>
       {iconList.map((value) => {
         return (
-          <Icon
+          <div
+            style={{
+              width: 110,
+              height: 80,
+              display: "flex",
+              margin: 5,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "1px solid #a6a9b6 ",
+            }}
             key={value}
-            icon={value}
-            size={size}
-            color={fillColor}
-            style={{ display: "inline" }}
-            stroke="blue"
-          />
+          >
+            <Icon
+              icon={value}
+              size={size}
+              color={fillColor}
+              style={{ display: "inline" }}
+              stroke="blue"
+            />
+            <div
+              style={{
+                borderRadius: 4,
+                backgroundColor: "#f4f5db",
+                color: "#a6a9b6",
+                padding: 4,
+                fontSize: 12,
+                marginTop: 10,
+                width: "90%",
+                textAlign: "center",
+              }}
+            >
+              {value}
+            </div>
+          </div>
         );
       })}
     </div>
   );
 };
-
 
 export const AllStories = (): React.ReactElement<IconProps> => {
   // const iconList = select("Icon", IconList, IconList.LIKE);
