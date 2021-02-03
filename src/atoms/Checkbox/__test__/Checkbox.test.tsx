815f916add6cbec648fe957061e9d1f937e0220e
Le Tu Quyen <quyen.le@eastplayers.io>
2021-02-03 10:20:42 +0700

    Add test for checkbox

--- src/atoms/Checkbox/__test__/Checkbox.test.tsx
+++ src/atoms/Checkbox/__test__/Checkbox.test.tsx
@@ -2,16 +2,48 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Checkbox } from "..";
+import { CheckboxSizes } from "../types";
 
+// Test Label
 test("Checkbox expect has Testing label", async () => {
-  const { getByLabelText } = render(<Checkbox label="Testing" />);
-
-  expect(getByLabelText("Testing")).toBeInTheDocument();
-  expect(getByLabelText("Testing")).toHaveTextContent("Testing");
+  const { getByText } = render(<Checkbox label="Testing" />);
+  expect(getByText("Testing")).toBeInTheDocument();
+  expect(getByText("Testing")).toHaveTextContent("Testing");
 });
 
-test("Checkbox expect has Testing label", async () => {
-  const { container } = render(<Checkbox label="Testing" />);
+// Test Classes
+test("Checkbox expect to be disabled", async () => {
+  const { getByText } = render(<Checkbox disabled label="Testing" />);
+  expect(getByText("Testing")).toBeInTheDocument();
+  expect(getByText("Testing")).toHaveClass("disabled");
+});
 
-  fireEvent.click(container);
+test("Checkbox expect to has small size", async () => {
+  const { getByText } = render(
+    <Checkbox size={CheckboxSizes.SMALL} label="Testing" />
+  );
+  expect(getByText("Testing")).toBeInTheDocument();
+  expect(getByText("Testing")).toHaveClass("checkbox--small");
+});
+
+test("Checkbox expect to be inlined", async () => {
+  const { getByText } = render(<Checkbox inline label="Testing" />);
+  expect(getByText("Testing")).toBeInTheDocument();
+  expect(getByText("Testing")).toHaveClass("checkbox--inline");
+});
+
+// Test behaviours
+test("Checkbox expect to be checked/unchecked when clicking on label", async () => {
+  const { getByText } = render(<Checkbox label="Testing" />);
+  const label = getByText("Testing") as HTMLLabelElement;
+  fireEvent.click(label);
+  expect(label.querySelector("input")?.checked).toEqual(true);
+  fireEvent.click(label);
+  expect(label.querySelector("input")?.checked).toEqual(false);
+});
+
+test("Checkbox expect to be checked be default", async () => {
+  const { getByText } = render(<Checkbox defaultChecked label="Testing" />);
+  const label = getByText("Testing");
+  expect(label.querySelector("input")?.checked).toEqual(true);
 });
