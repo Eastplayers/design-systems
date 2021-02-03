976e0d47b3443ec4cabe247e648c1f9785517747
Le Tu Quyen <quyen.le@eastplayers.io>
2021-02-03 16:31:55 +0700

    Add radio buttons

---
+++ src/atoms/Radio/Radio.stories.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { withKnobs, text, select, boolean } from "@storybook/addon-knobs";
+import "@storybook/addon-knobs/register";
+
+import { Radio } from ".";
+import { RadioProps, RadioSizes } from "./types";
+
+export const All = (): React.ReactElement<RadioProps> => {
+  const label = text("Label", "Sample content");
+  const size = select("Weight", RadioSizes, RadioSizes.NORMAL);
+  const disabled = boolean("Disabled", false);
+  const inline = boolean("Inline", false);
+
+  return (
+    <div>
+      <Radio
+        label={label}
+        name="testing"
+        size={size}
+        disabled={disabled}
+        inline={inline}
+      />
+      <Radio
+        label={label}
+        name="testing"
+        size={size}
+        disabled={disabled}
+        inline={inline}
+      />
+    </div>
+  );
+};
+
+export default {
+  title: "Atoms/Radio",
+  decorators: [withKnobs]
+};
