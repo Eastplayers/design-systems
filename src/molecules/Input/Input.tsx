73ff55b3a9bf87932b087d53cf011758adfcb976
esp-phudang <phu.dang@eastplayers.io>
2021-03-10 11:25:52 +0700

    refactor code, add mid-part div, add className for Input comp

--- src/molecules/Input/Input.tsx
+++ src/molecules/Input/Input.tsx
@@ -61,21 +61,27 @@ const Input: React.FC<InputProps> = (props) => {
     setFocus(false);
   };
 
-  const inputBorder = classNames(styles["input-border"], {
-    [styles["input-border-paragraph"]]: type === TextInputType.PARAGRAPH,
-    [styles["input-border-filled"]]: filled,
-    [styles["input-border-disabled"]]: disabled,
-    [styles["input-border-focus"]]: isFocus,
-    [styles["input-border-error"]]: !disabled && error
-  });
+  const inputBorder =
+    classNames(styles["input-border"], {
+      [styles["input-border-mid-part-paragraph"]]:
+        type === TextInputType.PARAGRAPH,
+      [styles["input-border-state-filled"]]: filled,
+      [styles["input-border-state-disabled"]]: disabled,
+      [styles["input-border-state-focus"]]: isFocus,
+      [styles["input-border-state-error"]]: !disabled && error
+    }) +
+    " " +
+    className;
+  //can put className for Input Component
   const inputLabel = classNames(styles["input-label"], {
-    [styles["input-label-disabled"]]: disabled,
-    [styles["input-label-error"]]: !disabled && error
+    [styles["input-label-state-disabled"]]: disabled,
+    [styles["input-label-state-error"]]: !disabled && error
   });
   const inputHelperText = classNames(styles["input-helper-text"], {
-    [styles["input-helper-text-error"]]: error,
-    [styles["input-helper-text-disabled"]]: disabled
+    [styles["input-helper-text-state-error"]]: error,
+    [styles["input-helper-text-state-disabled"]]: disabled
   });
+
   return (
     <>
       {labelStyle === InputLabelStyles.TOP_LABEL && (
@@ -89,22 +95,22 @@ const Input: React.FC<InputProps> = (props) => {
       <div className={inputBorder}>
         <Icon
           size="22"
-          className={styles["input-border-icon-leading"]}
+          className={styles["input-icon-leading"]}
           // will not display icon if input type is paragraph
           icon={type === TextInputType.PARAGRAPH ? "" : leadingIcon}
           //import disabledColor $gray400 from scss
           color={disabled ? styles.disabledColor : iconColor}
         />
-        <div style={{width:"100%"}}>
+        <div className={styles["input-border-mid-part"]}>
           {labelStyle === InputLabelStyles.CONTAINED_LABEL && (
             <label className={inputLabel} htmlFor={label}>
               {label}
             </label>
           )}
-          <div className={styles["input-border-content"]}>
+          <div className={styles["input-border-mid-part-content"]}>
             <Icon
               size="18"
-              className={styles["input-border-icon-prefix"]}
+              className={styles["input-icon-prefix"]}
               icon={type === TextInputType.PARAGRAPH ? "" : prefixIcon}
               color={disabled ? styles.disabledColor : iconColor}
             />
@@ -119,11 +125,10 @@ const Input: React.FC<InputProps> = (props) => {
               onBlur={OnBlurHandler}
               style={textStyle}
               disabled={disabled}
-              className={className}
             />
             <Icon
               size="18"
-              className={styles["input-border-icon-suffix"]}
+              className={styles["input-icon-suffix"]}
               icon={type === TextInputType.PARAGRAPH ? "" : suffixIcon}
               color={disabled ? styles.disabledColor : iconColor}
             />
@@ -131,7 +136,7 @@ const Input: React.FC<InputProps> = (props) => {
         </div>
         <Icon
           size="22"
-          className={styles["input-border-icon-trailing"]}
+          className={styles["input-icon-trailing"]}
           icon={type === TextInputType.PARAGRAPH ? "" : trailingIcon}
           color={disabled ? styles.disabledColor : iconColor}
         />
