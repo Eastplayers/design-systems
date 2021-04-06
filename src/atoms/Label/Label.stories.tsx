a71b4c5ee28ab33640a4ea74040168db374e62bf
esp-phudang <phu.dang@eastplayers.io>
2021-04-06 17:41:25 +0700

    split label to atom

---
+++ src/atoms/Label/Label.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { text, withKnobs } from "@storybook/addon-knobs";
+import "@storybook/addon-knobs/register";
+import Label, { LableProps } from "./Label";
+
+export const All: React.FC<LableProps> = () => {
+  const value = text("Label value", "Label");
+  return <Label value={value} />;
+};
+
+export default {
+  title: "Atoms/Label",
+  decorators: [withKnobs]
+};
